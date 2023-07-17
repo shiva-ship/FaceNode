db9f1105e2dcb42123c5091ac8de5c93d2f55ae6
Rastosh <shivansh.rastogi19@vit.edu>
2023-07-18 00:33:40 +0530

    face capture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaceDetectionComponent } from './face-detection/face-detection.component';
+import { WebcamModule  } from 'ngx-webcam';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceDetectionComponent
+    // WebcamComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,  
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
